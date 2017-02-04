e5c66aa36c3f0cd4206b9cc6a669e68f470ced56
nikang <mohamadnikan2013@gmail.com>
2017-02-04 12:29:11 +0330

    Marinenemy added

---
+++ Marinenemy.h
@@ -0,0 +1,17 @@
+//
+// Created by nikang on 1/31/17.
+//
+
+#ifndef MARINENEMY_H
+#define MARINENEMY_H
+
+#include"EnemyObject.h"
+
+class EnemyObejct;
+
+class Marinenemy : public EnemyObejct{
+
+};
+
+
+#endif //MARINENEMY_H
